aa7a004484480d61cbead148013a3e69fd103a27
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2026-02-06 15:47:32 -0500

    Refactor recyclarr.bash for folder-based configuration

--- scripts/recyclarr.bash
+++ scripts/recyclarr.bash
@@ -1,38 +1,23 @@
-getArrAppInfo () {
-  # Get Arr App information
-  if [ -z "$arrUrl" ] || [ -z "$arrApiKey" ]; then
-	arrUrlBase="$(sed -n '/<UrlBase>/{s/.*<UrlBase>//;s/<\/UrlBase.*//;p;}' /radarr/config.xml)"
-    if [ "$arrUrlBase" == "null" ]; then
-      arrUrlBase=""
-    else
-      arrUrlBase="/$(echo "$arrUrlBase" | sed "s/\///")"
-    fi
-	arrName="$(sed -n '/<InstanceName>/{s/.*<InstanceName>//;s/<\/InstanceName.*//;p;}' /radarr/config.xml)"
-	arrApiKey="$(sed -n '/<ApiKey>/{s/.*<ApiKey>//;s/<\/ApiKey.*//;p;}' /radarr/config.xml)"
-	arrPort="$(sed -n '/<Port>/{s/.*<Port>//;s/<\/Port.*//;p;}' /radarr/config.xml)"
-    arrUrl="http://$arrName:${arrPort}${arrUrlBase}"
-  fi
-}
-
 Process () {
 
 	folder="$1"
-	getArrAppInfo
+	
+	# Get Arr App information
+	arrApiKey="$(sed -n '/<ApiKey>/{s/.*<ApiKey>//;s/<\/ApiKey.*//;p;}' /$folder/config.xml)"
 
-	echo "Configuring recyclarr for $arrName"
-	recyclarrConfigFile="/recyclarr/configs/${arrName}.yaml"
+	echo "Configuring recyclarr for $1"
+	recyclarrConfigFile="/recyclarr/configs/${folder}.yaml"
 	if [ -f "$recyclarrConfigFile" ]; then
 		rm "$recyclarrConfigFile"
 	fi
-	cp /git-sync/configs/recyclarr/radarr.yaml "$recyclarrConfigFile"
+	cp /git-sync/configs/recyclarr/$folder.yaml "$recyclarrConfigFile"
 	if [ -f "$recyclarrConfigFile" ]; then
-		sed -i "s%instance_name%$arrName%g" "$recyclarrConfigFile"
-		sed -i "s%enter_url%$arrUrl%g" "$recyclarrConfigFile"
-		sed -i "s%enter_api_key%$arrApiKey%g" "$recyclarrConfigFile"
+		sed -i "s%api_key%$arrApiKey%g" "$recyclarrConfigFile"
 	fi
 }
 
-Process "/radarr"
+Process "radarr"
+Process "sonarr"
 
 
 

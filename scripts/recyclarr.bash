0496af3f2a0b71841653f1f6ac6cc107ff566a0e
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2026-02-06 11:17:42 -0500

    Implement getArrAppInfo function
    Add function to retrieve Arr App information from config.

--- scripts/recyclarr.bash
+++ scripts/recyclarr.bash
@@ -1,1 +1,20 @@
+getArrAppInfo () {
+  # Get Arr App information
+  if [ -z "$arrUrl" ] || [ -z "$arrApiKey" ]; then
+    arrUrlBase="$(cat /config/config.xml | xq | jq -r .Config.UrlBase)"
+    if [ "$arrUrlBase" == "null" ]; then
+      arrUrlBase=""
+    else
+      arrUrlBase="/$(echo "$arrUrlBase" | sed "s/\///")"
+    fi
+    arrName="$(cat /config/config.xml | xq | jq -r .Config.InstanceName)"
+    arrApiKey="$(cat /config/config.xml | xq | jq -r .Config.ApiKey)"
+    arrPort="$(cat /config/config.xml | xq | jq -r .Config.Port)"
+    arrUrl="http://127.0.0.1:${arrPort}${arrUrlBase}"
+  fi
+}
 
+getArrAppInfo
+# Configure Yaml with URL and API Key
+sed -i "s%arrUrl%$arrUrl%g" "/recyclarr/$arrName.yaml"
+sed -i "s%arrApi%$arrApiKey%g" "/recyclarr/$arrName.yaml"

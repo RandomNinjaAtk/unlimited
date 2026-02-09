66777e23b05bdbb21004fae76a50e7fc0a544994
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2026-02-09 16:24:22 -0500

    Update radarr.bash

--- scripts/radarr.bash
+++ scripts/radarr.bash
@@ -16,4 +16,11 @@ curl -s 'http://unlimited_radarr:7878/api/v3/config/mediamanagement' \
   -H "X-Api-Key: $arrApiKey" \
   --data-raw '{"autoUnmonitorPreviouslyDownloadedMovies":false,"recycleBin":"","recycleBinCleanupDays":7,"downloadPropersAndRepacks":"doNotPrefer","createEmptyMovieFolders":false,"deleteEmptyFolders":false,"fileDate":"none","rescanAfterRefresh":"always","autoRenameFolders":false,"pathsDefaultStatic":false,"setPermissionsLinux":false,"chmodFolder":"755","chownGroup":"","skipFreeSpaceCheckWhenImporting":true,"minimumFreeSpaceWhenImporting":1000,"copyUsingHardlinks":true,"useScriptImport":false,"scriptImportPath":"","importExtraFiles":false,"extraFileExtensions":"srt","enableMediaInfo":false,"id":1}'
 
-  exit
+echo "Configure Metadata"
+curl 'http://unlimited_radarr:7878/api/v3/metadata/1?' \
+  -X 'PUT' \
+  -H 'Content-Type: application/json' \
+  -H "X-Api-Key: $arrApiKey" \
+  --data-raw '{"enable":true,"name":"Kodi (XBMC) / Emby","fields":[{"name":"movieMetadata","value":true},{"name":"useMovieNfo","value":true},{"name":"movieMetadataLanguage","value":1},{"name":"movieMetadataURL","value":false},{"name":"addCollectionName","value":true},{"name":"movieImages","value":true}],"implementationName":"Kodi (XBMC) / Emby","implementation":"XbmcMetadata","configContract":"XbmcMetadataSettings","infoLink":"https://wiki.servarr.com/radarr/supported#xbmcmetadata","tags":[],"id":1}' 
+
+exit

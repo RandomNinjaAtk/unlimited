1ab4edf2664cb75a4d4ce1100e2626bf283f91bf
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2026-02-09 16:29:39 -0500

    Update sonarr.bash

--- scripts/sonarr.bash
+++ scripts/sonarr.bash
@@ -16,4 +16,18 @@ curl -s 'http://unlimited_sonarr:8989/api/v3/config/mediamanagement' \
   -H "X-Api-Key: $arrApiKey" \
   --data-raw '{"autoUnmonitorPreviouslyDownloadedMovies":false,"recycleBin":"","recycleBinCleanupDays":7,"downloadPropersAndRepacks":"doNotPrefer","createEmptyMovieFolders":false,"deleteEmptyFolders":false,"fileDate":"none","rescanAfterRefresh":"always","autoRenameFolders":false,"pathsDefaultStatic":false,"setPermissionsLinux":false,"chmodFolder":"755","chownGroup":"","skipFreeSpaceCheckWhenImporting":true,"minimumFreeSpaceWhenImporting":1000,"copyUsingHardlinks":true,"useScriptImport":false,"scriptImportPath":"","importExtraFiles":false,"extraFileExtensions":"srt","enableMediaInfo":false,"id":1}'
 
-  exit
+echo "Configure Metadata"
+curl 'http://192.168.10.10:8999/api/v3/metadata/4?' \
+  -X 'PUT' \
+  -H 'Content-Type: application/json' \
+  -H "X-Api-Key: $arrApiKey" \
+  --data-raw '{"enable":true,"name":"Plex","fields":[{"name":"seriesPlexMatchFile","value":true},{"name":"episodeMappings","value":true}],"implementationName":"Plex","implementation":"PlexMetadata","configContract":"PlexMetadataSettings","infoLink":"https://wiki.servarr.com/sonarr/supported#plexmetadata","tags":[],"id":4}'
+  
+  
+curl 'http://192.168.10.10:8999/api/v3/metadata/1?' \
+  -X 'PUT' \
+  -H 'Content-Type: application/json' \
+  -H "X-Api-Key: $arrApiKey" \
+  --data-raw '{"enable":true,"name":"Kodi (XBMC) / Emby","fields":[{"name":"seriesMetadata","value":true},{"name":"seriesMetadataEpisodeGuide","value":false},{"name":"seriesMetadataUrl","value":false},{"name":"episodeMetadata","value":true},{"name":"episodeImageThumb","value":true},{"name":"seriesImages","value":true},{"name":"seasonImages","value":true},{"name":"episodeImages","value":true}],"implementationName":"Kodi (XBMC) / Emby","implementation":"XbmcMetadata","configContract":"XbmcMetadataSettings","infoLink":"https://wiki.servarr.com/sonarr/supported#xbmcmetadata","tags":[],"id":1}'
+  
+exit

af56f4a7c0baa055a851ba0ec16b25c90e1c413f
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2026-03-05 13:30:25 -0500

    Update recyclarr.bash

--- scripts/recyclarr.bash
+++ scripts/recyclarr.bash
@@ -1,20 +1,19 @@
-version="1.2"
+version="1.3"
 scriptName="Recyclarr"
 Process () {
 
-	folder="$1"
 	
 	# Get Arr App information
-	arrApiKey="$(sed -n '/<ApiKey>/{s/.*<ApiKey>//;s/<\/ApiKey.*//;p;}' /$folder/config.xml)"
+	arrApiKey="$(sed -n '/<ApiKey>/{s/.*<ApiKey>//;s/<\/ApiKey.*//;p;}' /$1/config.xml)"
 
 	echo "Configuring recyclarr for $1"
-	recyclarrConfigFile="/recyclarr/configs/${folder}.yaml"
+	recyclarrConfigFile="/recyclarr/configs/${2}.yaml"
 	if [ -f "$recyclarrConfigFile" ]; then
 		rm "$recyclarrConfigFile"
 	fi
 	cp /config/$folder.yaml "$recyclarrConfigFile"
 	if [ -f "$recyclarrConfigFile" ]; then
-		sed -i "s%arr_api_key_${folder}%$arrApiKey%g" "$recyclarrConfigFile"
+		sed -i "s%arr_api_key_${1}%$arrApiKey%g" "$recyclarrConfigFile"
 	fi
 	chmod 777 "$recyclarrConfigFile"
 }
@@ -22,13 +21,12 @@ Process () {
 
 Includes () {
 	echo "Importing $1 include files"
-	folder="$1"
-	recyclarrConfigFile="/recyclarr/includes/${folder}_custom_formats.yaml"
-	if [ -f /config/${folder}_custom_formats.yaml ]; then
+	recyclarrConfigFile="/recyclarr/includes/${1}_custom_formats.yaml"
+	if [ -f /config/${1}_custom_formats.yaml ]; then
 		if [ -f "$recyclarrConfigFile" ]; then
 			rm "$recyclarrConfigFile"
 		fi
-		cp /config/${folder}_custom_formats.yaml "$recyclarrConfigFile"
+		cp /config/${1}_custom_formats.yaml "$recyclarrConfigFile"
 	fi
 	recyclarrConfigFile="/recyclarr/includes/${folder}_quality.yaml"
 	if [ -f /config/${folder}_quality.yaml ]; then
@@ -51,10 +49,10 @@ fi
 
 echo ""
 echo "$version :: $scriptName"
-Process "radarr-4k"
-Process "radarr"
+Process "radarr-4k" "radarr"
+Process "radarr" "radarr"
 Includes "radarr"
-Process "sonarr"
+Process "sonarr" "sonarr"
 Includes "Sonarr"
 
 

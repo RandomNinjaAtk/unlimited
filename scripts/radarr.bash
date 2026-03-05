bff5dc27117f8d9eb36de6e773dd947acf386f79
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2026-03-05 12:40:22 -0500

    Update radarr.bash

--- scripts/radarr.bash
+++ scripts/radarr.bash
@@ -1,23 +1,24 @@
 # Get Arr App information
-arrApiKey="$(sed -n '/<ApiKey>/{s/.*<ApiKey>//;s/<\/ApiKey.*//;p;}' /radarr/config.xml)"
+
+arrApiKey="$(sed -n '/<ApiKey>/{s/.*<ApiKey>//;s/<\/ApiKey.*//;p;}' /$1/config.xml)"
 
 echo "Configure Root Folder"
-curl -s 'http://unlimited_radarr:7878/api/v3/rootFolder' \
+curl -s "http://unlimited_$1:7878/api/v3/rootFolder" \
   -X 'POST' \
   -H 'Content-Type: application/json' \
   -H "X-Api-Key: $arrApiKey" \
-  --data-raw '{"path":"/mnt/user/appdata/unlimited/data/media/radarr/"}'
+  --data-raw "{\"path\":\"/mnt/user/appdata/unlimited/data/media/$1/\"}"
   
 
 echo "Configure Settings"
-curl -s 'http://unlimited_radarr:7878/api/v3/config/mediamanagement' \
+curl -s "http://unlimited_$1:7878/api/v3/config/mediamanagement" \
   -X 'PUT' \
   -H 'Content-Type: application/json' \
   -H "X-Api-Key: $arrApiKey" \
   --data-raw '{"autoUnmonitorPreviouslyDownloadedMovies":false,"recycleBin":"","recycleBinCleanupDays":7,"downloadPropersAndRepacks":"doNotPrefer","createEmptyMovieFolders":false,"deleteEmptyFolders":false,"fileDate":"none","rescanAfterRefresh":"always","autoRenameFolders":false,"pathsDefaultStatic":false,"setPermissionsLinux":false,"chmodFolder":"755","chownGroup":"","skipFreeSpaceCheckWhenImporting":true,"minimumFreeSpaceWhenImporting":1000,"copyUsingHardlinks":true,"useScriptImport":false,"scriptImportPath":"","importExtraFiles":false,"extraFileExtensions":"srt","enableMediaInfo":false,"id":1}'
 
 echo "Configure Metadata"
-curl 'http://unlimited_radarr:7878/api/v3/metadata/1?' \
+curl "http://unlimited_$1:7878/api/v3/metadata/1?" \
   -X 'PUT' \
   -H 'Content-Type: application/json' \
   -H "X-Api-Key: $arrApiKey" \

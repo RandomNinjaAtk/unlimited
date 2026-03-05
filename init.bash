3fb31fdaafeca1d992a03c055abc80af6f91d69f
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2026-03-05 12:54:05 -0500

    Update init.bash

--- init.bash
+++ init.bash
@@ -6,6 +6,7 @@ curl "https://raw.githubusercontent.com/RandomNinjaAtk/unlimited/refs/heads/main
 
 echo "Downloading Configs"
 curl "https://raw.githubusercontent.com/RandomNinjaAtk/unlimited/refs/heads/main/configs/recyclarr/radarr.yaml" -o /config/radarr.yaml
+curl "https://raw.githubusercontent.com/RandomNinjaAtk/unlimited/refs/heads/main/configs/recyclarr/includes/radarr_custom_formats.yaml" -o /config/radarr_custom_formats.yaml
 curl "https://raw.githubusercontent.com/RandomNinjaAtk/unlimited/refs/heads/main/configs/recyclarr/sonarr.yaml" -o /config/sonarr.yaml
 
 echo "Setting Permissions:"
